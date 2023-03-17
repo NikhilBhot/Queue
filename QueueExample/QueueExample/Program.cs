bc828d405ce9c9349a9ecf3a335f66a8b0eddec0
unknown <bhotnikhil25@gmail.com>
2023-03-17 16:44:04 +0530

    [Nikhil]-Queue

--- QueueExample/QueueExample/Program.cs
+++ QueueExample/QueueExample/Program.cs
@@ -5,6 +5,12 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Wel-Come TO Queue Data Structure!");
+            QueueLinkedList queueLinkedList=new QueueLinkedList();
+            queueLinkedList.Enqueue(56);
+            queueLinkedList.Enqueue(30);
+            queueLinkedList.Enqueue(70);
+            queueLinkedList.Display();
+            Console.ReadKey();
         }
     }
 }
